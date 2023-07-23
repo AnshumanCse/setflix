0ab282a812d2db7d29c40ae4728af5b798f67454
Anshuman Tiwari <112653339+AnshumanCse@users.noreply.github.com>
2023-07-23 14:51:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ const apiPaths = {
     `${apiEndpoint}/discover/movie?api_key=${apikey}&with_genres=${id}`,
   fetchTrending: `${apiEndpoint}/trending/all/day?api_key=${apikey}&language=en-US`,
   searchOnYoutube: (query) =>
-    `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyC0SZJkHFX-fQ7NrsxdI4l4mGwYuY4l7P8`,
+    `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=YOUR_YOUTUBE_API_KEY`,
 };
 
 // Boots up the app
-function init() {
+const init = () => {
   fetchTrendingMovies();
   fetchAndBuildAllSections();
-}
+};
 
-function fetchTrendingMovies() {
+const fetchTrendingMovies = () => {
   fetchAndbuildMovieSection(apiPaths.fetchTrending, "Trending Now")
     .then((list) => {
       const randomIndex = parseInt(Math.random() * list.length);
@@ -27,9 +27,9 @@ function fetchTrendingMovies() {
     .catch((err) => {
       console.error(err);
     });
-}
+};
 
-function buildBannerSection(movie) {
+const buildBannerSection = (movie) => {
   const bannerCont = document.getElementById("banner-section");
 
   bannerCont.style.backgroundImage = `url('${imgPath}${movie.backdrop_path}')`;
@@ -37,26 +37,24 @@ function buildBannerSection(movie) {
   const div = document.createElement("div");
 
   div.innerHTML = `
-            <h2 class="banner__title">${movie.title}</h2>
-            <p class="banner__info">Trending in movies | Released - ${
-              movie.release_date
-            } </p>
-            <p class="banner__overview">${
-              movie.overview && movie.overview.length > 200
-                ? movie.overview.slice(0, 200).trim() + "..."
-                : movie.overview
-            }</p>
-            <div class="action-buttons-cont">
-                <button class="action-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path d="M4 2.69127C4 1.93067 4.81547 1.44851 5.48192 1.81506L22.4069 11.1238C23.0977 11.5037 23.0977 12.4963 22.4069 12.8762L5.48192 22.1849C4.81546 22.5515 4 22.0693 4 21.3087V2.69127Z" fill="currentColor"></path></svg> &nbsp;&nbsp; Play</button>
-                <button class="action-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3ZM1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM13 10V18H11V10H13ZM12 8.5C12.8284 8.5 13.5 7.82843 13.5 7C13.5 6.17157 12.8284 5.5 12 5.5C11.1716 5.5 10.5 6.17157 10.5 7C10.5 7.82843 11.1716 8.5 12 8.5Z" fill="currentColor"></path></svg> &nbsp;&nbsp; More Info</button>
-            </div>
-        `;
+    <h2 class="banner__title">${movie.title}</h2>
+    <p class="banner__info">Trending in movies | Released - ${movie.release_date}</p>
+    <p class="banner__overview">${
+      movie.overview && movie.overview.length > 200
+        ? movie.overview.slice(0, 200).trim() + "..."
+        : movie.overview
+    }</p>
+    <div class="action-buttons-cont">
+      <button class="action-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path d="M4 2.69127C4 1.93067 4.81547 1.44851 5.48192 1.81506L22.4069 11.1238C23.0977 11.5037 23.0977 12.4963 22.4069 12.8762L5.48192 22.1849C4.81546 22.5515 4 22.0693 4 21.3087V2.69127Z" fill="currentColor"></path></svg> &nbsp;&nbsp; Play</button>
+      <button class="action-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3ZM1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM13 10V18H11V10H13ZM12 8.5C12.8284 8.5 13.5 7.82843 13.5 7C13.5 6.17157 12.8284 5.5 12 5.5C11.1716 5.5 10.5 6.17157 10.5 7C10.5 7.82843 11.1716 8.5 12 8.5Z" fill="currentColor"></path></svg> &nbsp;&nbsp; More Info</button>
+    </div>
+  `;
   div.className = "banner-content container";
 
   bannerCont.append(div);
-}
+};
 
-function fetchAndBuildAllSections() {
+const fetchAndBuildAllSections = () => {
   fetch(apiPaths.fetchAllCategories)
     .then((res) => res.json())
     .then((res) => {
@@ -72,9 +70,9 @@ function fetchAndBuildAllSections() {
       // console.table(movies);
     })
     .catch((err) => console.error(err));
-}
+};
 
-function fetchAndbuildMovieSection(fetchUrl, categoryName) {
+const fetchAndbuildMovieSection = (fetchUrl, categoryName) => {
   console.log(fetchUrl, categoryName);
   return fetch(fetchUrl)
     .then((res) => res.json())
@@ -87,9 +85,9 @@ function fetchAndbuildMovieSection(fetchUrl, categoryName) {
       return movies;
     })
     .catch((err) => console.error(err));
-}
+};
 
-function buildMoviesSection(list, categoryName) {
+const buildMoviesSection = (list, categoryName) => {
   console.log(list, categoryName);
 
   const moviesCont = document.getElementById("movies-cont");
@@ -105,11 +103,11 @@ function buildMoviesSection(list, categoryName) {
     .join("");
 
   const moviesSectionHTML = `
-        <h2 class="movie-section-heading">${categoryName} <span class="explore-nudge">Explore All</span></span></h2>
-        <div class="movies-row">
-            ${moviesListHTML}
-        </div>
-    `;
+    <h2 class="movie-section-heading">${categoryName} <span class="explore-nudge">Explore All</span></span></h2>
+    <div class="movies-row">
+      ${moviesListHTML}
+    </div>
+  `;
 
   const div = document.createElement("div");
   div.className = "movies-section";
@@ -117,9 +115,9 @@ function buildMoviesSection(list, categoryName) {
 
   // append html into movies container
   moviesCont.append(div);
-}
+};
 
-function searchMovieTrailer(movieName, iframId) {
+const searchMovieTrailer = (movieName, iframId) => {
   if (!movieName) return;
 
   fetch(apiPaths.searchOnYoutube(movieName))
@@ -136,11 +134,11 @@ function searchMovieTrailer(movieName, iframId) {
       elements.append(div);
     })
     .catch((err) => console.log(err));
-}
+};
 
-window.addEventListener("load", function () {
+window.addEventListener("load", () => {
   init();
-  window.addEventListener("scroll", function () {
+  window.addEventListener("scroll", () => {
     // header ui update
     const header = document.getElementById("header");
     if (window.scrollY > 5) header.classList.add("black-bg");
